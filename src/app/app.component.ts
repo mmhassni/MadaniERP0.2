b507a30067886a4742659efacb3fd98f13dcfc0f
mmhassni <mahssni.mohammed@gmail.com>
2019-05-14 23:54:08 +0100

    second deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
 
-  rootPage: any = GestionPaiementChoixActionPage;
+  rootPage: any = ListeProjetPage;
 
   pages: Array<{title: string, component: any}>;
 
